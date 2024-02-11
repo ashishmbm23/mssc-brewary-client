333f5ad08c9a1d1464ec157891d0abd010280867
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-02-11 12:54:16 +0530

    Error handling implementation.

--- src/main/java/com/ashish/msscbrewaryclient/web/v1/BeerDto.java
+++ src/main/java/com/ashish/msscbrewaryclient/web/v1/BeerDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -20,4 +22,8 @@ public class BeerDto {
     private String beerName;
     private String beerStyle;
     private Long upc;
+    private Long quantityOnHand;
+    private BigDecimal price;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastUpdatedDate;
 }
--- src/test/java/com/ashish/msscbrewaryclient/client/BrewaryClientTest.java
+++ src/test/java/com/ashish/msscbrewaryclient/client/BrewaryClientTest.java
@@ -4,9 +4,12 @@ import com.ashish.msscbrewaryclient.web.v1.BeerDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.net.URI;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,21 +18,42 @@ class BrewaryClientTest {
 
     @Autowired
     BrewaryClient brewaryClient;
+
+    @Value("${test.uuid.value:546baee0-c9e2-4a93-803b-e2ff8d4398d4}")
+    private String id ;
+
     @BeforeEach
     void setUp() {
     }
 
     @Test
     void getBeerById() {
-        BeerDto beerDto = brewaryClient.getBeerById(UUID.randomUUID());
+        UUID uuid = UUID.fromString(id);
+        BeerDto beerDto = brewaryClient.getBeerById(uuid);
         assertNotNull(beerDto);
     }
 
     @Test
     void saveBeer(){
-        BeerDto beerDto = BeerDto.builder().beerName("Pilsner").upc(11231321331l).build();
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Pilsner")
+                .upc(11231321331l)
+                .quantityOnHand(100l)
+                .beerStyle("LAGER")
+                .createdDate(OffsetDateTime.of(2024, 2, 10, 12, 0, 0, 0, ZoneOffset.UTC))
+                .lastUpdatedDate(OffsetDateTime.of(2024, 2, 10, 12, 0, 0, 0, ZoneOffset.UTC))
+                .build();
         URI uri = brewaryClient.saveBeer(beerDto);
         assertNotNull(uri);
         System.out.println(uri.toString());
+    }
+
+    @Test
+    void beanValidation(){
+        BeerDto beerDto = BeerDto.builder()
+                .id(UUID.randomUUID())
+                .beerName("Pilsner")
+                .build();
+        brewaryClient.saveBeer(beerDto);
     }
 }
